2b240866062411b28a333f5142f27133923de731
Benjamin Eckstein <>
2021-07-02 11:40:50 +0200

    first TDD Session

--- src/main/java/com/github/benjamineckstein/calculator/Calculator.java
+++ src/main/java/com/github/benjamineckstein/calculator/Calculator.java
@@ -1,4 +1,10 @@
 package com.github.benjamineckstein.calculator;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Calculator {
 
     /**
@@ -8,7 +14,7 @@ public class Calculator {
      * "1+1" --> 2
      * "2*2" --> 4
      * "2/4" --> 0.5
-     *
+     * <p>
      * Supported operators are +, -, *, /, ()
      * Numbers can be formatted integers or doubles like 1.023,5501
      *
@@ -16,7 +22,40 @@ public class Calculator {
      * @return
      */
     public static double calculate(String input) {
-        return 0;
+
+        if (Objects.isNull(input)) {
+            input = "0";
+        }
+
+        List<String> mathExpressions = getMathExpressions(input);
+        return calculateMathExpressions(mathExpressions);
+    }
+
+    public static double calculateMathExpressions(List<String> mathExpressions) {
+
+        mathExpressions = new ArrayList<>(mathExpressions);
+
+        double calculatedValue = Integer.parseInt(mathExpressions.remove(0));
+        while (!mathExpressions.isEmpty()) {
+            mathExpressions.remove(0);
+            double nextValue = Integer.parseInt(mathExpressions.remove(0));
+            calculatedValue = calculatedValue + nextValue;
+        }
+        return calculatedValue;
+    }
+
+    public static List<String> getMathExpressions(String input) {
+
+        String[] sumParts = input.split("\\+");
+
+        List<String> parts = new ArrayList<>();
+
+        for (String sumPart : sumParts) {
+            parts.add(sumPart);
+            parts.add("+");
+        }
+        parts.remove(parts.size() - 1);
+        return parts;
     }
 
 }
--- src/test/java/com/github/benjamineckstein/calculator/CalculatorTest.java
+++ src/test/java/com/github/benjamineckstein/calculator/CalculatorTest.java
@@ -1,13 +1,81 @@
 package com.github.benjamineckstein.calculator;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 class CalculatorTest {
 
     @Test
     void calculate() {
         double calculate = Calculator.calculate("0");
-        assertEquals(0,calculate);
+        assertThat(calculate).isEqualTo(0);
     }
+
+    @Test
+    void calculateNull() {
+        double calculate = Calculator.calculate(null);
+        assertThat(calculate).isEqualTo(0);
+    }
+
+    @Test
+    void calculateSimpleNumberNotZero() {
+        double calculate = Calculator.calculate("1");
+        assertThat(calculate).isEqualTo(1);
+    }
+
+    @Test
+    void calculateSimpleNumberNegativ() {
+        double calculate = Calculator.calculate("-10");
+        assertThat(calculate).isEqualTo(-10);
+    }
+
+    @Test
+    void calculateSimpleAddition() {
+        double calculate = Calculator.calculate("1+1");
+        assertThat(calculate).isEqualTo(2);
+    }
+
+
+    @Test
+    void getMathExpressions() {
+
+        String input = "1";
+        List<String> mathExpressions = Calculator.getMathExpressions(input);
+        assertThat(mathExpressions).isNotNull().isNotEmpty().contains("1");
+    }
+
+    @Test
+    void getMathExpressionsForASum() {
+        String input = "1+1";
+        List<String> mathExpressions = Calculator.getMathExpressions(input);
+        assertThat(mathExpressions).containsExactly("1", "+", "1");
+    }
+
+    @Test
+    void getMathExpressionsForABigSum() {
+        String input = "20+90";
+        List<String> mathExpressions = Calculator.getMathExpressions(input);
+        assertThat(mathExpressions).containsExactly("20", "+", "90");
+    }
+
+
+    @Test
+    void calculateMathExpressions() {
+        //given
+        List<String> expressions =List.of("20","+","90");
+
+        //when
+        double result = Calculator.calculateMathExpressions(expressions);
+
+        //then
+        assertThat(result).isEqualTo(110);
+    }
+
+
 }
